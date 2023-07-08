bbc8c709b93f64010e8fd34d7bb677fbbb7d00e3
warm20 <du_lingli@163.com>
2023-07-08 17:32:28 +0800

    commit files

---
+++ server/ser_kurle.py
@@ -0,0 +1,12 @@
+import socket as sok
+from threading import Thread
+
+class Server:
+    def __init__(self, host, port) -> None:
+        self.server = sok.socket()
+        self.host = host
+        self.port = port
+        self.server.bind((self.host, self.port))
+
+    def run(self) -> None:
+        ...
