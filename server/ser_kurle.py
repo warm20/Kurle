d09c3f8d0f1fd6ca4946bbe4f05b4add7b480922
warm20 <du_lingli@163.com>
2023-07-08 18:55:39 +0800

    req

--- server/ser_kurle.py
+++ server/ser_kurle.py
@@ -8,10 +8,21 @@ class Server:
         self.port = port
         self.server.bind((self.host, self.port))
 
+    def SetLogic(self, logics:dict):
+        self.logics = logics
+
     def run(self) -> None:
         self.server.listen()
         while True:
             c,addr = self.server.accept()
             req = c.recv(1024)
             hdrs = req.split(b"\r\n")
-            
+            path = hdrs[0].split()[1]
+            res = ""
+            try:
+                self.logics[path](res)
+            except:
+                print("Not write this logic ")
+                res = b"HTTP/1.0 404 NOT FOUND\r\n\r\n Not logic"
+
+            c.sendall(res)
